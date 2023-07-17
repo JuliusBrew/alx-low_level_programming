8c8682470fe6c828a320dfe5f0b81da636db7bdc
JuliusBrew <ifern626@gmail.com>
2023-07-17 09:37:50 -0700

    prints all the numbers of base 16 in lowercase, followed by a new line

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - Prints numbers of hex 
+ * Return: Success
+ */
+
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 16; num++) 
+	{
+		if (num < 10) 
+		{
+			putchar(num + '0');
+		}
+		else 
+		{
+			char hChar = 'A' + (num - 10);
+			putchar(hChar);
+		}
+	}
+	putchar('\n');
+}
