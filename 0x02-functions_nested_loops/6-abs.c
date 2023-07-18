4ba1e755ed2449a2cfedd8ee65bc3d486065207d
JuliusBrew <ifern626@gmail.com>
2023-07-18 11:24:21 -0700

    computes the absolute value of an integer

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *_abs - returns 1 if alphabet is lower case
+ * @c: The character to print
+ * Return: Always (Success)
+ *
+ */
+
+int _abs(int n)
+{
+	if (n < 0)
+	{
+		return (n * -1);
+	}
+	return (n);
+
+	_putchar('\n');
+}
